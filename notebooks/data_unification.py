a335fb0c3f0ce652697acf50e6101935d5219c9c
nachosiguenza <nacho.siguenza@gmail.com>
2022-05-02 20:30:04 +0200

    Nuevos datos y documentacion adjunta

---
+++ notebooks/data_unification.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  2 19:53:45 2022
+
+@author: Nacho
+"""
+
+import pandas as pd
+import numpy as np
+import os
+
+os.chdir('X:\\Academico\\CUNEF\\TFM\\predictive_maintenance\\src')
